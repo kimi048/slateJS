b1292425f5242d91fdde34ba1f681b2e2825b2a6
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2022-05-20 22:35:41 +0900

    WIP example tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import logo from './logo.svg'
+
 import './App.css'
 import { htmlToSlateAST } from '@graphcms/html-to-slate-ast';
 
+import React, { useCallback, useMemo } from 'react';
+import { Editable, withReact, useSlate, Slate } from 'slate-react';
+import {
+  Editor,
+  Transforms,
+  createEditor,
+  Descendant,
+  Element as SlateElement,
+} from 'slate';
+import { withHistory } from 'slate-history';
+import RichText from "./Editor";
 
 async function App() {
 
@@ -13,33 +25,7 @@ async function App() {
   console.log(ast);}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <RichText/>
     </div>
   )
 }
