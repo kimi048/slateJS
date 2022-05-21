015ccd2a28fb9313bf0ab6ed2fdd4202d7f0e389
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2022-05-21 23:55:26 +0900

    demo state

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 // import { useState } from 'react'
 import logo from './logo.svg'
-
+import React from "react"
 import './App.css'
 import { htmlToSlateAST } from '@graphcms/html-to-slate-ast';
 
 import RichText from "./Editor";
 
-async function App() {
+function App() {
 
   // const [count, setCount] = useState(0);
-  const htmlString = '<div><div><p>test</p>a</div><p>test</p></div>'; // or import from a file or database
-  const ast = await htmlToSlateAST(htmlString);
-  if(ast){
-  console.log(ast);}
+  // const htmlString = '<div><div><p>test</p>a</div><p>test</p></div>'; // or import from a file or database
+  // const ast = await htmlToSlateAST(htmlString);
+  // if(ast){
+  // console.log(ast);}
   return (
     <div className="App">
+      hoghoge
       <RichText/>
     </div>
   )
